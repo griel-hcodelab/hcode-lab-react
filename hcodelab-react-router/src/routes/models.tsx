95d9135c53168b5ec0a4da66a650c0fee476a2c2
robertogriel <roberto@griel.com.br>
2022-04-09 15:14:07 -0300

    Criando a rota de modelos

--- hcodelab-react-router/src/routes/models.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Models = () => {
-  return (
-    <h1>Rota de Models</h1>
-  )
-}
-
-export default Models;
---
+++ hcodelab-react-router/src/routes/models/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import './models.scss';
+
+const Models = () => {
+	return (
+
+		<section id="models">
+			<h2>Models</h2>
+
+			<div className="models">
+
+
+				<div className="model-item">
+					<h3>718</h3>
+				</div>
+
+				<div className="model-item">
+					<h3>911</h3>
+				</div>
+
+				<div className="model-item">
+					<h3>Taycan</h3>
+				</div>
+
+				<div className="model-item">
+					<h3>Panamera</h3>
+				</div>
+
+				<div className="model-item">
+					<h3>Macan</h3>
+				</div>
+
+				<div className="model-item">
+					<h3>Cayenne</h3>
+				</div>
+
+			</div>
+
+		</section>
+
+	)
+}
+
+export default Models;
