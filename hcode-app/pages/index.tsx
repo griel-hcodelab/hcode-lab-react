a85feac687e445d35270cdeef6a5b56543f59db4
robertogriel <roberto@griel.com.br>
2022-04-16 16:42:49 -0300

    Passagem de dados

--- hcode-app/pages/index.tsx
+++ hcode-app/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Link href={`/blog/criando-slug-de-pagina`}>
+        <a>Link para o Post</a></Link>
+    </>
   )
 }
 
