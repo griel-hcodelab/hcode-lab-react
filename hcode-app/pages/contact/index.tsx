00acac658fa9a4e71f24cb7d85700f579d407e1e
robertogriel <roberto@griel.com.br>
2022-04-16 16:18:28 -0300

    Iniciando NextJS e Primeira Página

---
+++ hcode-app/pages/contact/index.tsx
@@ -0,0 +1,13 @@
+const ComponentPage = () => {
+
+    return (
+        <>
+        <form>
+            <input type="text" name="name" placeholder="Name" />
+            <button>Enviar</button>
+        </form>
+        </>
+    );
+}
+
+export default ComponentPage;
