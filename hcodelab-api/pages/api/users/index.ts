f82284e6aaf1227731ae6e152539ec41f6bb7acb
robertogriel <roberto@griel.com.br>
2022-04-21 11:22:02 -0300

    Api com o NextJS

---
+++ hcodelab-api/pages/api/users/index.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handlerUser(req: NextApiRequest, res: NextApiResponse){
+
+    switch(req.method){
+        case 'GET':
+            res.status(200).send('Conectado')
+        break;
+        case 'POST':
+            res.status(201).send('Criou com sucesso')
+        break;
+        case 'DELETE':
+            res.status(204).end();
+        break;
+    }
+}
